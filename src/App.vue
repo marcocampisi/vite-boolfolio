552f46f3a48deabc70679d5f9f70fc7891f06946
marcocampisi <marco.campisi@outlook.it>
2023-09-27 17:39:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Hello Vue</h1>
+</template>
+
+<style scoped>
+h1 {
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 20px 0;
+}
+</style>
