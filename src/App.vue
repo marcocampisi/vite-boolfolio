3e44d12351772c42bc3d89a2b5bd92b3b6f678a4
marcocampisi <marco.campisi@outlook.it>
2023-09-27 18:26:08 +0200

    (Milestone 5) Add card component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,35 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
+    components: ProjectCard,
     data() {
         return {
             projects: []
-        }
+        };
     },
     created() {
         axios.get('http://localhost:8000/api/projects')
             .then(response => {
-                this.projects = response.data.projects;
-                console.log(this.projects)
-            })
+            this.projects = response.data.projects;
+            console.log(this.projects);
+        })
             .catch(error => {
-                console.error('Errore nella chiamata Axios:', error)
-            })
-    }
+            console.error('Errore nella chiamata Axios:', error);
+        });
+    },
+    components: { ProjectCard }
 }
 </script>
 
 <template>
     <div>
         <h1>Lista dei Progetti</h1>
-        <ul>
-            <li v-for="project in projects" :key="project.id">
-                <h3>{{ project.title }}</h3>
-                <p>{{ project.description }}</p>
-                <p>{{ project.date }}</p>
-            </li>
-        </ul>
+        <div v-for="project in projects" :key="project.id">
+            <ProjectCard :project="project"></ProjectCard>
+        </div>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object,
+    }
+}
+</script>
+
+
+<template >
+    <div class="project-card">
+        <h3>{{ project.title }}</h3>
+        <p>{{ project.description }}</p>
+        <p>{{ project.date }}</p>
+    </div>
+</template>
+
+<style scoped>
+.project-card {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px;
+}
+</style>
